568a651f537ff657f5d8d503a731d5aa8d1ac910
Merkona <austin.r.merkel@gmail.com>
2025-11-24 23:48:33 -0600

    test

--- RCSJParams.py
+++ RCSJParams.py
@@ -105,3 +105,5 @@ class RCSJ:
                 raise AttributeError(f"Unknown parameter: {name}")
             setattr(self.params, name, value)
         self.params.compute_dimensionless()
+
+print("test")
